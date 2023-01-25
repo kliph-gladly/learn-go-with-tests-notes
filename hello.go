d24c7d4c957dcc42afc924d9b6a596b8468b1efb
kliph-gladly <111882901+kliph-gladly@users.noreply.github.com>
2023-01-25 12:13:28 -0500

    Extract helper function for mapping language to prefix

--- hello.go
+++ hello.go
@@ -9,20 +9,25 @@ const ENGLISH_HELLO_PREFIX = "Hello, "
 const SPANISH_HELLO_PREFIX = "Hola, "
 const GERMAN_HELLO_PREFIX = "Hallo, "
 
-func Hello(name string, language string) string {
-	if name == "" {
-		name = "World"
-	}
-
-	prefix := ENGLISH_HELLO_PREFIX
-
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case SPANISH_LANGUAGE:
 		prefix = SPANISH_HELLO_PREFIX
 	case GERMAN_LANGUAGE:
 		prefix = GERMAN_HELLO_PREFIX
+	default:
+		prefix = ENGLISH_HELLO_PREFIX
 	}
 
+	return prefix
+}
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := greetingPrefix(language)
 	return fmt.Sprintf("%s%s", prefix, name)
 }
 
