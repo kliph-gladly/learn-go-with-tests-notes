8cdbdb0dc5aabc25c2c738c4a7aa2cfdc875065f
kliph-gladly <111882901+kliph-gladly@users.noreply.github.com>
2023-03-22 11:14:18 -0400

    Example of indirect dependencies

--- learn/uuid.go
+++ learn/uuid.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-
-	"github.com/google/uuid"
+	"github.com/gocolly/colly"
 )
 
 func main() {
-	uuidWithHyphen, _ := uuid.NewRandom()
-	uuid := strings.Replace(uuidWithHyphen.String(), "-", "", -1)
-	fmt.Println(uuid)
+	_ = colly.NewCollector()
 }
