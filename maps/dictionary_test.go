48e9e5f77e9bdbfc408eea80d21cd2659e32b069
kliph-gladly <111882901+kliph-gladly@users.noreply.github.com>
2023-04-26 16:16:34 -0400

    Add dictionary example for maps

---
+++ maps/dictionary_test.go
@@ -0,0 +1,45 @@
+package dictionary
+
+import "testing"
+
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t *testing.T, err, want error) {
+	t.Helper()
+
+	if err == nil {
+		t.Fatal("expected to get an error.")
+	}
+
+	if err != want {
+		t.Errorf("got %q want %q", err, want)
+	}
+}
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertError(t, err, ErrNotFound)
+	})
+}
