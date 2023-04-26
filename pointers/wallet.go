eeb1faac7b6c6aa7061ad85ad4752349d3073c9e
kliph-gladly <111882901+kliph-gladly@users.noreply.github.com>
2023-04-26 11:03:44 -0400

    Use pointers to update referenced values in a struct

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,1 +1,22 @@
 package pointers
+
+import "fmt"
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
