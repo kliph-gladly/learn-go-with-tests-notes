84af8715b5f723791923f5f51c77fde31c8b2b4b
kliph-gladly <111882901+kliph-gladly@users.noreply.github.com>
2023-05-24 13:51:11 -0400

    Fix linter errors

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -26,7 +26,6 @@ func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
 	t.Run("known word", func(t *testing.T) {
-
 		got, _ := dictionary.Search("test")
 		want := "this is just a test"
 
